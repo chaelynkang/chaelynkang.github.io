c2e1b2f64a83900b2c50e741c40d7dd4a6924b1c
chaelynKang <128279031+chaelynKang@users.noreply.github.com>
2023-04-18 11:34:51 +0900

    p

---
+++ python_work/0418-code02-06.py
@@ -0,0 +1,17 @@
+import turtle
+
+## 함수
+
+## 변수
+myT = None
+
+## 메인 코드
+myT = turtle.Turtle()
+myT.shape('turtle')
+
+for i in range(0, 4) :
+    myT.forward(200)
+    myT.right(90)
+
+
+turtle.done()
