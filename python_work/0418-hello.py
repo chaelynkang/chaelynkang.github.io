28df1a16b0f0b3447c3558ecc915ad9f53eb666f
chaelynKang <128279031+chaelynKang@users.noreply.github.com>
2023-04-18 11:15:19 +0900

    p

---
+++ python_work/0418-hello.py
@@ -0,0 +1,22 @@
+print("계산기")
+#a=100
+#b=300
+a = int(input("첫번째 숫자를 입력하세요 : "))
+b = int(input("두번째 숫자를 입력하세요 : "))
+'''
+# print(result)
+print(str(a) + "+" + str(b) + "=" + str(result))
+'''
+result = a + b
+print(a, "+",b,"=",result)
+result = a - b
+print(a,"-",b,"=",result)
+result = a * b
+print(a,"X",b,"=",result)
+result = a / b
+print(a,"/",b,"=",result)
+result = a % b
+print(a,"%",b,"=",result)
+result = a // b
+print(a,"//",b,"=",result)
+
