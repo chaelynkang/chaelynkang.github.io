efbfad80eb41e62ef0d09d0fe705770a7446e926
chaelynKang <128279031+chaelynKang@users.noreply.github.com>
2023-04-19 17:06:53 +0900

    ss

--- python_work/0419-code06-061.py
+++ python_work/0419-code06-061.py
@@ -2,7 +2,29 @@
 #    for j in range(0, 2, 1):
 #        print("변수의 변화를 확인 : ", i, j)
 
-for dan in range(2, 10):
-    print("%d 단" % dan)
-    for k in range(1, 10):
-        print("%d X %d = %d" %(dan, k, dan*k))
+# for dan in range(2, 10):
+#    print("%d 단" % dan)
+#    for k in range(1, 10):
+#        print("%d X %d = %d" %(dan, k, dan*k))
+#for i in range(2, 10):
+#    print(" %d 단" %i,end= " ")
+#print("")    
+#for dan in range(2, 10):
+#    print("%d 단" % dan)
+#    for k in range(1, 10):
+#        print("%d X %d = %d" %(dan, k, dan*k))
+#for i in range(2, 10):
+#    print(" %d 단" %i,end='')
+#print("")    
+#for k in range(1, 10):
+#    #print("%d 단" % dan)
+#    for dan in range(2, 10):
+#        print("%2dX%2d=%2d " %(dan, k, dan*k), end='')
+#    print()
+
+
+for k in range(10, 1):
+    #print("%d 단" % dan)
+    for dan in range(10, 2):
+        print("%dX%d=%d " %(dan, k, dan*k), end='')
+    print()
