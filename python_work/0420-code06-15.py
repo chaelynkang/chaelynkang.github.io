20c4e86f804298e75c5c1c8993cead1be60cab5a
chaelynKang <128279031+chaelynKang@users.noreply.github.com>
2023-04-20 10:08:58 +0900

    ,

--- python_work/0420-code06-15.py
+++ python_work/0420-code06-15.py
@@ -5,20 +5,16 @@ while i < 9 :
     if i < 5 :
         k = 0
         while k < 4 - i :
-            print(' ', end = '')
+            print(' ', end = ' ')
             k += 1
         k = 0
         while k < i * 2 + 1 :
-            print('\u2665', end = '')
+            print('\u2665', end = ' ')
             k += 1
     else :
-        k = 0
-        while k < i - 4 :
-            print(' ', end = '')
-            k += 1
-        k = 0
-        while k < (9 - i) * 2 - 1 :
-            print('\u2665', end = '')
-            k += 1
+        for k in range(0,i-4):
+            print (' ', end=' ')
+        for k in range((9-i)*2-1) :
+            print('\u2665', end=' ')
     print()
     i += 1
